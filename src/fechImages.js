fcb62a9816dfee98f4e5cfed6f99d647b1493618
lgutie16 <lindaandgod@gmail.com>
2019-10-07 10:08:44 -0500

    displaying images

--- src/fechImages.js
+++ src/fechImages.js
@@ -1,5 +1,5 @@
-export default function fetchImages() {
-    return fetch("https://picsum.photos/v2/list")
+export default function fetchImages(id) {
+    return fetch(`https://picsum.photos/id/${id}/info`)
         .then(res => res.json())
         .catch((err) => console.log(err))
 }
--- src/routes/Gallery/Gallery.js
+++ src/routes/Gallery/Gallery.js
@@ -1,48 +1,75 @@
 import React, { useState, useEffect, Suspense } from 'react'
-//import { unstable_createResource as createResource } from 'react-cache'
-import InViewMonitor from 'react-inview-monitor';
+import ErrorBoundary from 'react-error-boundary'
+import { unstable_createResource as createResource } from 'react-cache'
+import fetchImages from '../../fechImages'
 
 
-/* const ImageResource = createResource(
-    source =>
-        new Promise(resolve => {
-            const img = new Image();
-            img.src = source;
-            img.onload = resolve;
-        })
-); */
+const cache = {}
 
-const Img = ({ src, alt, ...props }) => {
-    //ImageResource.read(src);
-    //return <img src={src} alt={alt} {...props} width="300" height="300" />
-    return <div> {src} </div>
+
+const LoadImage = createResource((src) => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener("load", () => resolve(img));
+    img.addEventListener("error", err => reject(err));
+    img.src = src;
+}))
+
+const RenderImage = ({ src }) => {
+    LoadImage.read(src)
+    return <img src={src} />
 }
 
-const ImageWrapper = ({ image, nr, render }) => {
-    return render ? <Suspense fallback="loading">
-        <Img src={image.download_url} alt={`img_large_${nr}`} />
-    </Suspense> : <div></div>
+
+const FetchImage = ({ id }) => {
+    const image = cache[id]
+    if (!image) {
+        const promise = fetchImages(id).then(
+            image => (cache[image.id] = image),
+        )
+        throw promise
+    }
+
+    return <div>
+        {JSON.stringify(image || 'Unknown', null, 2)}
+
+        <ErrorBoundary FallbackComponent={() => 'There was an error...'}>
+            <Suspense fallback="loading...">
+                <RenderImage src={image.download_url} />
+            </Suspense>
+        </ErrorBoundary>
+    </div>
+}
+
+const ImageInfo = ({ id }) => {
+    return <ErrorBoundary FallbackComponent={() => 'There was an error...'}>
+        <Suspense fallback="loading...">
+            <FetchImage id={id} />
+        </Suspense>
+    </ErrorBoundary>
 }
 
 const Gallery = () => {
-    const [hasError, setErrors] = useState(false)
-    const [images, setImages] = useState([])
+    const [id, setId] = useState(null)
 
-    useEffect(() => {
-        fetch("https://picsum.photos/v2/list?page=1&limit=10")
-            .then(res => res.json())
-            .then(res => setImages(res))
-            .catch(() => setErrors(true))
+    function handleSubmit(e) {
+        e.preventDefault()
+        setId(e.target.elements.id.value)
     }
-    );
 
-    return <div>
-        {images.map((image, i) =>
-            <InViewMonitor key={i} childPropsInView={{ render: true }}>
-                <ImageWrapper image={image} nr={i} />
-            </InViewMonitor>
-        )}
-    </div>
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="id-input">Id</label>
+                <input id="id-input" name="id" />
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                {id && <ImageInfo id={id} />}
+            </div>
+
+        </div>
+    )
+
 }
 
 export default Gallery
